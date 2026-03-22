f6bd7d5504f07cb7945a89ad5f55a5799aacd3a8
manuelkogei13-rgb <manuelkogei13@gmail.com>
2026-03-23 01:57:52 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE = "emms-v1";
+self.addEventListener("install", e =>
+  e.waitUntil(caches.open(CACHE).then(c =>
+    c.addAll(["./emms.html","./manifest.json"])
+  ))
+);
+self.addEventListener("fetch", e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
